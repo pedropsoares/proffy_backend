c0c69be5af7018c5fd56254e243ef33ce8b7ec62
pedropsoares <pedropaulos.1998@gmail.com>
2020-08-08 12:04:10 -0300

    create method linked to the route

--- src/routes.ts
+++ src/routes.ts
@@ -1,7 +1,9 @@
 import express from 'express';
+import ClassesController from './controllers/ClassesController';
 
 const routes = express.Router();
+const classesController = new ClassesController();
 
-routes.post('/classes');
+routes.post('/classes', classesController.create);
 
 export default routes;
